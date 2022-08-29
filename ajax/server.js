dc212b79f14bb8583dd562338c5bf2ae3dec1c13
gustavomrv <gustavomourago@gmail.com>
2022-08-29 11:05:23 -0300

    function ajax

--- ajax/server.js
+++ ajax/server.js
@@ -6,5 +6,5 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste', (req, res) => res.send('Ok'))
+// app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Estás a funcionar...'))
