32046be6f9f4733594d9ac1c50b87b28469843f4
gustavomrv <gustavomourago@gmail.com>
2022-08-28 21:29:38 -0300

    Update flappy.js

---
+++ desafios/flappy_bird/dom/js/flappy.js
@@ -0,0 +1,9 @@
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
+}
+
+function Barreira(reversa = false) {
+    this.elemento = novoElemento('div', 'barreira')
+}
