03a51a69d8784a9cb59c6f304f895ccc168e6fb8
gustavomrv <gustavomourago@gmail.com>
2022-08-28 21:44:52 -0300

    Update flappy.js

--- desafios/flappy_bird/dom/js/flappy.js
+++ desafios/flappy_bird/dom/js/flappy.js
@@ -36,4 +36,12 @@ function ParDeBarreiras(altura, abertura, x) {
     }
 
     this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => this.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
 }
+
+const b = new ParDeBarreiras(700,200,400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
