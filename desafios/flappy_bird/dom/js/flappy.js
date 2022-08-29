40d2d73652333204b4fd3a08bb4e39a0cf2d03c0
gustavomrv <gustavomourago@gmail.com>
2022-08-28 21:41:43 -0300

    Update flappy.js

--- desafios/flappy_bird/dom/js/flappy.js
+++ desafios/flappy_bird/dom/js/flappy.js
@@ -15,6 +15,25 @@ function Barreira(reversa = false) {
     this.setAltura = altura => corpo.style.height = `${altura}px`
 }
 
-const b = new Barreira(true)
-b.setAltura(300)
-document.querySelector('[wm-flappy]').appendChild(b.elemento)
+// const b = new Barreira(true)
+// b.setAltura(300)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function ParDeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento['div', 'par-de-barreiras']
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const alturaSuperior = Math.random() * (altura - abertura)
+        const alturaInferior = altura - abertura - alturaSuperior
+        this.superior.setAltura(alturaSuperior)
+        this.inferior.setAltura(alturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+}
