05f33fc7c3c8fa753ec2c2ec7baa0e9516af40f4
gustavomrv <gustavomourago@gmail.com>
2022-08-28 22:47:20 -0300

    Update flappy.js

--- desafios/flappy_bird/dom/js/flappy.js
+++ desafios/flappy_bird/dom/js/flappy.js
@@ -100,11 +100,21 @@ function Passaro(alturaJogo) {
     this.setY(alturaJogo / 2)
 }
 
+
+
+function Progresso() {
+    this.elemento = novoElemento('span', 'progresso');
+    this.atualizarPontos = pontos => {
+        this.elemento.innerHTML = pontos
+    }
+    this.atualizarPontos(0)
+}
 const barreiras = new Barreiras(700,1200,200,400)
 const passaro = new Passaro(700)
 const areaDoJogo = document.querySelector('[wm-flappy]')
 
 areaDoJogo.appendChild(passaro.elemento)
+areaDoJogo.appendChild(new Progresso().elemento)
 barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
 setInterval(() => {
     barreiras.animar()
