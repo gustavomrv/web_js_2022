74705ef8bf94353f49f9ae2e64444d34d3d20f84
gustavomrv <gustavomourago@gmail.com>
2022-08-28 21:33:53 -0300

    Update flappy.js

--- desafios/flappy_bird/dom/js/flappy.js
+++ desafios/flappy_bird/dom/js/flappy.js
@@ -6,4 +6,15 @@ function novoElemento(tagName, className) {
 
 function Barreira(reversa = false) {
     this.elemento = novoElemento('div', 'barreira')
+
+    const borda = novoElemento('div', 'borda')
+    const corpo = novoElemento('div', 'corpo')
+    this.elemento.appendChild(reversa ? corpo : borda)
+    this.elemento.appendChild(reversa ? borda : corpo)
+
+    this.setAltura = altura => corpo.style.height = `${altura}px`
 }
+
+const b = new Barreira(true)
+b.setAltura(300)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
