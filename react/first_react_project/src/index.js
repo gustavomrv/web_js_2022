477c65b1968b40651435a6b441f3b720c8f6a694
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-10 20:06:26 -0300

    Update index.js

--- react/first_react_project/src/index.js
+++ react/first_react_project/src/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>React</h1>, document.getElementById('root'))
+import First from './components/first'
+
+ReactDOM.render(<First />, document.getElementById('root'))
