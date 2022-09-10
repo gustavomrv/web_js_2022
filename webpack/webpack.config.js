b606d217fa9ab55a8ce3cd20a132c7ec9a87a43a
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-10 16:21:57 -0300

    npm fix

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,9 +1,11 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
+
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
