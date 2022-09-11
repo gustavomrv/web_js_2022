eb12dd510ce7eeb41085e35de63c14aaec80bfcb
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 17:49:08 -0300

    Update Calculator.jsx

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import './Calculator.css'
 
+import Button from "../components/Button";
+
 export default class Calculator extends Component {
     render() {
         return <div className="calculator">
-                
-            
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+            <Button />
         </div>
         
     }
