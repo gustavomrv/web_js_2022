f94c839dd35f6d8d0f1a0331c9012f6fdfd326b3
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 11:35:52 -0300

    Update saudacao.jsx

--- react/first_react_project/src/components/saudacao.jsx
+++ react/first_react_project/src/components/saudacao.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 
 export default class Saudacao extends React.Component {
+setTipo(e) {
+    this.props.tipo = e.target.value
+}
+
     render() {
         const { tipo, nome} = this.props
         return (
             <div>
                 <h1>{tipo} {nome}!</h1>
                 <hr />
-                <input type="text" placeholder="Tipo..." value={tipo} /> 
+                <input type="text" placeholder="Tipo..." value={tipo} 
+                onChange={e => this.setTipo(e)} /> 
                 <input type="text" placeholder="Nome..." value={nome} />
             </div>  
         )
