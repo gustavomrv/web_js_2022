3f6193975a644da1667d179c4e54c6128d89bb88
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-10 21:08:34 -0300

    Create saudacao.jsx

---
+++ react/first_react_project/src/components/saudacao.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default class Saudacao extends React.Component {
+    render() {
+        const { tipo, nome} = this.props
+        return (
+            <div>
+                <h1>{tipo} {nome}!</h1>
+                <hr />
+                <input type="text" placeholder="Tipo..." value={tipo} /> 
+                <input type="text"
+            </div>  
+        )
+    }
+}
