27c3179183260cdbdf9e37d1e70a85b0fd02797f
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 22:42:56 -0300

    Update Calculator.jsx

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -28,7 +28,9 @@ export default class Calculator extends Component {
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true})
+        }
     }
 
     addDigit(n) {
