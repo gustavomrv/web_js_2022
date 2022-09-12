b2bf26fdd95728185a1c4eb328db26c3a56e0b60
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 22:27:19 -0300

    Update Calculator.jsx

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -4,8 +4,18 @@ import './Calculator.css'
 import Button from "../components/Button";
 import Display from "../components/Display";
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
     
+    state = { ...initialState }
+
     constructor(props) {
         super(props)
         this.clearMemory = this.clearMemory.bind(this)
