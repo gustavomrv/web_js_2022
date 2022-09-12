c52fd32c42c30f3ea28ea2fbcc65747cda1963f8
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 22:05:45 -0300

    Update Button.jsx

--- react/calculator/src/components/Button.jsx
+++ react/calculator/src/components/Button.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import './Button.css'
 
-export default props =>
-    <button className="button">{props.label}</button>
+export default props => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    return (
+    <button 
+        onClick={e => props.click && props.click(props.label)}
+        className={classes}>        
+        {props.label}
+    </button>
+    )
+}
