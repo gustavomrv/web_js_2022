82ffe2190fd6dd07b19e7768e0d01962b9d57ef9
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-11 22:21:12 -0300

    Update Calculator.jsx

--- react/calculator/src/main/Calculator.jsx
+++ react/calculator/src/main/Calculator.jsx
@@ -28,23 +28,23 @@ export default class Calculator extends Component {
     render() {
         return <div className="calculator">
             <Display value={100} />
-            <Button label="AC" click={this.clearMemory}/>
-            <Button label="/" click={this.setOperation}/>
+            <Button label="AC" click={this.clearMemory} triple/>
+            <Button label="/" click={this.setOperation} operation/>
             <Button label="7" click={this.addDigit}/>
             <Button label="8" click={this.addDigit}/>
             <Button label="9" click={this.addDigit}/>
-            <Button label="*" click={this.setOperation}/>
+            <Button label="*" click={this.setOperation} operation/>
             <Button label="4" click={this.addDigit}/>
             <Button label="5" click={this.addDigit}/>
             <Button label="6" click={this.addDigit}/>
-            <Button label="-" click={this.setOperation}/>
+            <Button label="-" click={this.setOperation} operation/>
             <Button label="1" click={this.addDigit}/>
             <Button label="2" click={this.addDigit}/>
             <Button label="3" click={this.addDigit}/>
-            <Button label="+" click={this.setOperation}/>
-            <Button label="0" click={this.addDigit}/>
+            <Button label="+" click={this.setOperation} operation/>
+            <Button label="0" click={this.addDigit} double/>
             <Button label="." click={this.addDigit}/>
-            <Button label="=" click={this.setOperation}/>
+            <Button label="=" click={this.setOperation} operation/>
         </div>
         
     }
